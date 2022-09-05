4a70a90272fdd62a94a890e1d8e02efda6b1c9e0
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-09-05 19:21:48 +0200

    balazs-matyassy: space

--- src/hu/progmatic/Main.java
+++ src/hu/progmatic/Main.java
@@ -2,6 +2,7 @@ package hu.progmatic;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello Space!");
         System.out.println("Hello World!");
     }
 }
