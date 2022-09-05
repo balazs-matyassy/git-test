84d93671be46e468216b499cf5b92e05c263e653
Ria <benedek.maria88@gmail.com>
2022-09-05 19:29:50 +0200

    meow

--- src/hu/progmatic/Main.java
+++ src/hu/progmatic/Main.java
@@ -1,9 +1,5 @@
 package hu.progmatic;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Space!");
-        System.out.println("Hello World!");
-        System.out.println("Bye World!!!");
-    }
+
 }
